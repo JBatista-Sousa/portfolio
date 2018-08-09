920663fdf10c8a5239d5721754ab5f89a3071f07
werney <werney@outlook.com>
2018-08-08 21:45:28 -0300

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,39 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+</head>
+<body>
+        <div id="header-wrapper">
+                <div id="header" class="container">
+                    <div id="logo">
+                        <h1><a href="#">Portfolio</a></h1>
+                    </div>
+                    <div id="menu">
+                        <ul>
+                            <li><a href="#" accesskey="1" title="">Home</a></li>
+                            <li><a href="#" accesskey="2" title="">Cursos</a></li>
+                            <li><a href="#" accesskey="3" title="">Professores</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="banner" class="container">
+                    <div class="title">
+                        <h2>Porfolio do IFPI - Campus Paulistana</h2>
+                        <span class="byline">Seja Bem Vindo</span>
+                    </div>
+                    <ul class="actions">
+                        <li><a href="#" class="button">Conhecer</a></li>
+                    </ul>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+            <div id="copyright" class="container">
+                <p>&copy; IFPI. All rights reserved.</p>
+            </div>
+</body>
+</html>
