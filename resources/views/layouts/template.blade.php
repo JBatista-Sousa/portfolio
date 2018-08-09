8f4fa8532f4b185e0ca6f1255b6757e9b0656e24
werney <werney@outlook.com>
2018-08-09 17:03:38 -0300

    Merge template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+<<<<<<< HEAD
 
   <head>
 
@@ -83,8 +84,65 @@
       <div id="content-wrapper">
 
         <div class="container-fluid">
+
           @yield('conteudo')
 
+=======
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">-->
+  <!--<link href="assets/css/bootstrap.min.css" rel="stylesheet">--> 
+
+  <!--////////////////////////////////////////////////////////////////////////////////////////-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+   <!--<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">-->
+
+  <!--////////////////////////////////////////////////////////////////////////////////////////-->
+   
+  <!--<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">-->
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+</head>
+<body>
+
+<div class="container">
+
+  <div class="row">
+    <header class="page-header">
+      <h1> Portfólio | IFPI-CAPAU </h1>
+    </header>
+  </div>
+  
+  <nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 estilo">
+
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+              <a class="nav-link" href="">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{action('CursoController@cadastro')}}">Cursos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="{{action('DisciplinaController@cadastro')}}">Disciplinas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{action('PeriodoController@cadastro')}}">Periodos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="{{action('ProfessorController@cadastro')}}">Professores</a>
+            </li>
+          </ul>
+>>>>>>> db7dfc5d5bf4004740c16cca6277840a5978d155
         </div>
         <!-- /.container-fluid -->
 
